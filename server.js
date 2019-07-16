eaf345aa04bd4f5e07e20c733525c5b50ed648d1
Andrew <andrewyin1994@gmail.com>
2019-07-16 08:58:15 -0700

    completed sort by rating or app

--- server.js
+++ server.js
@@ -6,14 +6,25 @@ const app = express();
 app.use(morgan('common'));
 
 app.get('/apps',(req,res)=>{
-  const validSorts = ['rating','app'];
+  const validSorts = ['Rating','App'];
   const validGenre = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
   let results = [...playStore];
+  let sort;
 
+  if (req.query.sort) {
+    sort = req.query.sort;
+    sort = sort[0].toUpperCase() +  sort.slice(1);
+    if (validSorts.includes(sort)) {
+      results = results.sort((a,b) => a['Rating'] < b['Rating'] ? 1 : -1);
+    }
+  }
 
   res.status(200).json(results);
 
 });
+
+
+
 app.listen(8080, ()=>{
   console.log('server listening on port 8080');
 });
