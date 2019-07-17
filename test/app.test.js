cb6b8a1196eb9f961eae9ec960638d31589f02d7
Andrew <andrewyin1994@gmail.com>
2019-07-17 13:42:49 -0700

    successfully failed tests

--- test/app.test.js
+++ test/app.test.js
@@ -4,59 +4,76 @@ const supertest = require('supertest');
 const app = require('../app');
 
 describe('GET /apps', () => {
-  it('should return an array of games', () => {
-    return supertest(app)
+  it('should return an array of games', async () => {
+    const res = await supertest(app)
       .get('/apps')
       .expect(200)
       .expect('Content-Type', /json/)
-      .then(res => {
-        expect(res.body).to.be.an('array');
-        // expect(res.body).to.have.lengthOf.at.least(1);
-        res.body.forEach(app => {
-          expect(app).to.be.an('object');
-          expect(app).to.include.all.keys('App', 'Category', 'Rating', 'Reviews', 'Size', 'Installs', 'Type', 'Price', 'Content Rating', 'Genres', 'Last Updated', 'Current Ver', 'Android Ver');
-        });
-      });
+
+    expect(res.body).to.be.an('array');
+
+    for (let app of res.body) {
+      expect(app).to.be.an('object');
+      expect(app).to.include.all.keys('App', 'Category', 'Rating', 'Reviews', 'Size', 'Installs', 'Type', 'Price', 'Content Rating', 'Genres', 'Last Updated', 'Current Ver', 'Android Ver');
+    };
   });
 
-  it('sort should succeed on Rating', () => {
-    const validSort = ['rating', 'app'];
-    validSort.forEach(sort => {
-      return supertest(app)
+  it('sort should succeed on valid query', async () => {
+    const validSort = ['Rating', 'App'];
+
+    for (let sort of validSort) {
+      const res = await supertest(app)
         .get('/apps')
-        .query({sort})
+        .query({ sort })
         .expect(200);
-    });
+
+      expect(res.body).to.be.an('array');
+
+      let sorted = true;
+      if (sort === 'Rating') {
+        for (let i = 1; i < res.body.length; i++) {
+          sorted = sorted && res.body[i - 1][sort] >= res.body[i][sort];
+        }
+      }
+      else {
+        for (let i = 1; i < res.body.length; i++) {
+          sorted = sorted && res.body[i - 1][sort] < res.body[i][sort];
+        }
+      }
+      expect(sorted).to.be.true;
+    }
   });
 
   it('should fail on incorrect sort', () => {
     return supertest(app)
       .get('/apps')
-      .query({sort: 'cat'})
+      .query({ sort: 'cat' })
       .expect(400);
   });
 
-  it('genre should be one of "Action", "Puzzle", "Strategy", "Casual", "Arcade", "Card"', () => {
+
+  it('genre should be one of "Action", "Puzzle", "Strategy", "Casual", "Arcade", "Card"', async () => {
     const validGenres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
-    validGenres.forEach(genre => {
-      return supertest(app)
+
+    for (let genre of validGenres) {
+      const res = await supertest(app)
         .get('/apps')
-        .query({genre})
+        .query({ genre })
         .expect(200);
-    });
+
+      expect(res.body).to.be.an('array');
+
+      for (let app of res.body) {
+        expect(app['Genres']).to.include(genre);
+      }
+    }
   });
 
   it('should fail on incorrect genre', () => {
     return supertest(app)
       .get('/apps')
-      .query({genre: 'bat'})
+      .query({ genre: 'bat' })
       .expect(400);
   });
-
-  it('params should only be sort or genre', () => {
-    
-  });
-
-  it('should be 400 if sort is incorrect');
 
 });
